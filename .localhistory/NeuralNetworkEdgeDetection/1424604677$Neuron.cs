8d1a7de9baae6e9e38441b73b18f3c592a38e4eb
astartes91 <astartes91@gmail.com>
2015-02-22 21:25:15 +0600

    Initial import

---
+++ .localhistory/NeuralNetworkEdgeDetection/1424604677$Neuron.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetworkEdgeDetection
+{
+    class Neuron
+    {
+        public double[] Inputs { get; set; }
+        public double[] Weights { get; set; }
+
+        public Neuron[] Predecessors { get; set; }
+
+        //public Neuron(int number, bool input) { }
+
+        //todo: bias?
+        public double Output()
+        {
+            double x = 0;
+
+            for (int i = 0; i < Inputs.Count(); i++)
+            {
+                x += Inputs[i]*Weights[i];
+            }
+
+            double output = 1/(1 + Math.Exp(-x));
+            return output;
+        }
+    }
+}
---
+++ .localhistory/NeuralNetworkTrain/1424615340$Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NeuralNetworkEdgeDetection;
+
+namespace NeuralNetworkTrain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] inputPatterns = new int[,]
+            {
+                {0,0,0,0},
+                {0,0,0,1},
+                {0,0,1,0},
+                {0,0,1,1},
+                {0,1,0,0},
+                {0,1,0,1},
+                {0,1,1,0},
+                {0,1,1,1},
+                {1,0,0,0},
+                {1,0,0,1},
+                {1,0,1,0},
+                {1,0,1,1},
+                {1,1,0,0},
+                {1,1,0,1},
+                {1,1,1,0},
+                {1,1,1,1}
+            };
+
+            int[,] outputPatterns = new int[,]
+            {
+                {1,1,1,1},
+                {1,0,0,1},
+                {0,1,1,0},
+                {0,0,1,1},
+                {0,1,1,0},
+                {0,1,0,1},
+                {0,1,1,0},
+                {1,1,1,1},
+                {1,0,0,1},
+                {1,0,0,1},
+                {1,0,1,0},
+                {1,1,1,1},
+                {1,1,0,0},
+                {1,1,1,1},
+                {1,1,1,1},
+                {1,1,1,1}
+            };
+
+            Neuron inputNeuron1 = new Neuron();
+            inputNeuron1.Weights = new double[]{1};
+
+            Neuron inputNeuron2 = new Neuron();
+            inputNeuron2.Weights = new double[] { 1 };
+
+            Neuron inputNeuron3 = new Neuron();
+            inputNeuron3.Weights = new double[] { 1 };
+
+            Neuron inputNeuron4 = new Neuron();
+            inputNeuron4.Weights = new double[] { 1 };
+
+            Neuron hiddenNeuron1 = new Neuron();
+            hiddenNeuron1.Inputs = new double[]
+            {
+                inputNeuron1.Output(), inputNeuron2.Output(), inputNeuron3.Output(), inputNeuron4.Output()
+            };
+        }
+    }
+}
